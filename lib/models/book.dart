05b6ae6d13bd055091a201a14eaf59f21dd119af
rezeroyagami-bit <rezeroyagami@gmail.com>
2026-09-02 17:07:41 +0100

    Update book.dart

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -1,14 +1,34 @@
 class Book {
-  final String id, title, author, description, category, coverUrl, downloadUrl;
+  final String id;
+  final String title;
+  final String author;
+  final String? description;
+  final String category;
+  final String? coverUrl;
+  final String downloadUrl;
   final bool isMagazine;
+
   const Book({
     required this.id,
     required this.title,
     required this.author,
-    required this.description,
+    this.description,
     required this.category,
-    required this.coverUrl,
+    this.coverUrl,
     required this.downloadUrl,
     this.isMagazine = false,
   });
+
+  factory Book.fromMap(Map<String, dynamic> map) {
+    return Book(
+      id: map['id'] as String,
+      title: map['title'] as String,
+      author: map['author'] as String,
+      description: map['description'] as String?,
+      category: map['category'] as String,
+      coverUrl: map['cover_url'] as String?,
+      downloadUrl: map['download_url'] as String,
+      isMagazine: map['is_magazine'] as bool? ?? false,
+    );
+  }
 }
