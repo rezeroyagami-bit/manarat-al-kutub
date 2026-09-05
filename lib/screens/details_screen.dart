b5d78195e0ea39d1df4dc03df361ca057ff845c0
rezeroyagami-bit <rezeroyagami@gmail.com>
2026-09-05 20:07:19 +0100

    Fix book model import after theme color update

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../models/book.dart';
 import 'download_options_screen.dart';
 
 class DetailsScreen extends StatefulWidget {
