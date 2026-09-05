7d7b7652ed6d54e6874fe5c6143c1bd641776a78
rezeroyagami-bit <rezeroyagami@gmail.com>
2026-09-05 18:31:07 +0100

    Update coin balance instantly after downloads

--- lib/services/coins_service.dart
+++ lib/services/coins_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'device_identity_service.dart';
@@ -5,28 +6,34 @@ import 'supabase_service.dart';
 
 class CoinsService {
   static const _balanceKey = 'kitara_coins_balance';
+  static final ValueNotifier<int> balance = ValueNotifier<int>(0);
+
   final SupabaseService _supabase = SupabaseService();
 
   Future<int> loadBalance() async {
     try {
       final deviceId = await DeviceIdentityService.getId();
-      final balance = await _supabase.getCoinBalance(deviceId);
+      final value = await _supabase.getCoinBalance(deviceId);
       final prefs = await SharedPreferences.getInstance();
-      await prefs.setInt(_balanceKey, balance);
-      return balance;
+      await prefs.setInt(_balanceKey, value);
+      balance.value = value;
+      return value;
     } catch (_) {
       final prefs = await SharedPreferences.getInstance();
-      return prefs.getInt(_balanceKey) ?? 0;
+      final value = prefs.getInt(_balanceKey) ?? 0;
+      balance.value = value;
+      return value;
     }
   }
 
   Future<int?> awardDownloadCoin() async {
     try {
       final deviceId = await DeviceIdentityService.getId();
-      final balance = await _supabase.awardCoin(deviceId);
+      final newBalance = await _supabase.awardCoin(deviceId);
       final prefs = await SharedPreferences.getInstance();
-      await prefs.setInt(_balanceKey, balance);
-      return balance;
+      await prefs.setInt(_balanceKey, newBalance);
+      balance.value = newBalance;
+      return newBalance;
     } catch (_) {
       return null;
     }
